cd05de345bfce53e0b99496e51cc859d4102fcbc
camcoem <94968301+camcoem@users.noreply.github.com>
2022-06-28 18:07:14 +0200

    css solved, class component into functional

--- 5-min-react/src/Post.js
+++ 5-min-react/src/Post.js
@@ -1,4 +1,5 @@
-// import React from "react";
+import React, { Component, useState, useEffect } from "react";
+import "./App.css";
 
 // class Post extends Component {
 //   constructor(props) {
@@ -14,26 +15,72 @@
 //       .then((response) => response.json())
 //       .then((json) => this.setState({ posts: json }));
 //   }
-// }
 
-// export default function Post() {
-//   const { posts } = this.state;
+//   // export default function Post() {
+//   render() {
+//     const { posts } = this.state;
 
-//   return (
-//     <div className="container">
-//       <div class="jumbotron">
-//         <h1 class="display-4">Blog posts</h1>
-//       </div>
-//       {posts.map((post) => (
-//         <div className="card" key={post.id}>
-//           <div className="card-header">
-//             #{post.id} {post.title}
-//           </div>
-//           <div className="card-body">
-//             <p className="card-text">{post.body}</p>
-//           </div>
+//     return (
+//       <>
+//         <div className="jumbotron">
+//           <h1 className="display-4">Blog posts</h1>
 //         </div>
-//       ))}
-//     </div>
-//   );
+
+//         {posts.map((post) => (
+//           <div className="card" key={post.id}>
+//             <div className="card-header" style={{ backgroundColour: "black" }}>
+//               #{post.id} {post.title}
+//             </div>
+//             <div className="card-body">
+//               <p className="card-text">{post.body}</p>
+//             </div>
+//           </div>
+//         ))}
+//       </>
+//     );
+//   }
 // }
+
+const Post = () => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //the Usestate: allows us to track state in a function component.
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        setIsLoading(true);
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  //using useEffect: is order to be able to see the loader, and to add a dependency (which is the empty array). An empty array in this case meants that the loader would only appear once. Similar as in the case of classComponents with componentDidMount.
+
+  return (
+    <>
+      <div className="jumbotron">
+        <h1 className="display-4">Blog posts</h1>
+      </div>
+      {isLoading && <h2>Loading.......</h2>}
+      {posts.map((post) => (
+        <div className="card" key={post.id}>
+          <div className="card-header" style={{ backgroundColour: "black" }}>
+            #{post.id} {post.title}
+          </div>
+          <div className="card-body">
+            <p className="card-text">{post.body}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Post;
